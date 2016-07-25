d07cb255bebf91ddc44a4c2518cf3284695b6ee5
kantoku2 <sheema.sheemap@gmail.com>
2016-07-25 22:01:42 +0900

    first commit!

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+six_version = 'six>=1.9.0'
+install_requires = [
+    six_version,
+]
+
+setup(
+    name = 'hauth',
+    version = '0.0.1',
+    packages = ['hauth'],
+    description = '',
+    author = 'Daiki Shimada',
+    author_email = 'daiki.shimada.9g@stu.hosei.ac.jp',
+    url = 'https://gist.github.com/49f459da8a0522e05812.git',
+    install_requires = install_requires,
+    license='http://www.apache.org/licenses/LICENSE-2.0'
+    )
